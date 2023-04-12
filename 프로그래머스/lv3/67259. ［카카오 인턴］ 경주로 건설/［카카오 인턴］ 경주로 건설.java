7e58da4eb14d5f7e863dd60af719a6e0fbcc2273
Seo Yeji <55950992+syg9272@users.noreply.github.com>
2023-04-12 14:46:09 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 0.19 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/lv3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
+++ 프로그래머스/lv3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.java
@@ -7,9 +7,7 @@ class Solution {
     public int solution(int[][] board) {
         // 누적 가격을 저장할 2차원 배열
         int[][][] price = new int[board.length][board.length][4];
-        // 4방향 방문체크를 위한 3차원 배열
-        boolean[][][] visited = new boolean[board.length][board.length][4];
-        
+        // 최대 가격으로 초기화
         for(int i = 0; i < board.length; i++) {
             for(int j = 0; j < board.length; j++) {
                 Arrays.fill(price[i][j], Integer.MAX_VALUE);
@@ -19,14 +17,8 @@ class Solution {
         Queue<int[]> q = new ArrayDeque();
         // 처음 좌표, 갈 수 있는 방향 큐에 넣기 (우, 하)
         // {x좌표, y좌표, 방향, 누적가격}
-        // q.offer(new int[] {0, 0, 0, 0});
-        // q.offer(new int[] {0, 0, 1, 0});
         q.offer(new int[] {0, 0, 2, 0});
         q.offer(new int[] {0, 0, 3, 0});
-        visited[0][0][0] = true;
-        visited[0][0][1] = true;
-        visited[0][0][2] = true;
-        visited[0][0][3] = true;
         
         while(!q.isEmpty()) {
             int size = q.size();
@@ -35,26 +27,28 @@ class Solution {
                 for(int d = 0; d < 4; d++) {
                     int rx = temp[0] + dx[d];
                     int ry = temp[1] + dy[d];
+                    // 인덱스 유효하지 않거나 벽일 경우 continue
                     if(rx < 0 || rx >= board.length || ry < 0 || ry >= board.length || board[rx][ry] == 1) continue;
-                    // if((d == 0 && visited[rx][ry][2]) || (d == 0 && visited[rx][ry][2]) || (d == 0 && visited[rx][ry][2]) || (d == 0 && visited[rx][ry][2])) continue;
-                    // if((d == 0 && price[rx][ry][2] < temp[3] + 100) || (d == 0 && visited[rx][ry][2]) || (d == 0 && visited[rx][ry][2]) || (d == 0 && visited[rx][ry][2])) continue;
+                    // 같은 방향일 경우
                     if(temp[2] == d){
+                        // 해당 방향으로 이미 저장된 가격이 현재 가격보다 비싸지 않을 경우만 저장
                         if(price[rx][ry][d] >= temp[3] + 100) {
                             price[rx][ry][d] = temp[3] + 100;
-                            visited[rx][ry][d] = true;
                             q.offer(new int[] {rx, ry, d, temp[3] + 100});
                         }
                     }
+                    // 코너일 경우
                     if(temp[2] != d){
+                        // 해당 방향으로 이미 저장된 가격이 현재 가격보다 비싸지 않을 경우만 저장
                         if(price[rx][ry][d] >= temp[3] + 600) {
                             price[rx][ry][d] = temp[3] + 600;
-                            visited[rx][ry][d] = true;
                             q.offer(new int[] {rx, ry, d, temp[3] + 600});
                         }
                     }
                 }
             }
         }
+        // 도착지 4방향 중 가장 저렴한 가격 return
         int minPrice = Integer.MAX_VALUE;
         for(int d = 0; d < 4; d++) {
             minPrice = minPrice <= price[board.length - 1][board.length - 1][d] ? minPrice : price[board.length - 1][board.length - 1][d];
