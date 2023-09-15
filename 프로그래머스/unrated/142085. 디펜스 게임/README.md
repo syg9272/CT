0d8e8e4462d93559cb11835b53333548e4130279
Seo Yeji <syg9272@naver.com>
2023-09-15 21:21:35 +0900

    [unrated] Title: 디펜스 게임, Time: 0.76 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/unrated/142085. 디펜스 게임/README.md
+++ 프로그래머스/unrated/142085. 디펜스 게임/README.md
@@ -1,10 +1,10 @@
 # [unrated] 디펜스 게임 - 142085 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/142085) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/142085#) 
 
 ### 성능 요약
 
-메모리: 80.1 MB, 시간: 0.82 ms
+메모리: 76.3 MB, 시간: 0.76 ms
 
 ### 구분
 
--- 프로그래머스/unrated/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/unrated/142085. 디펜스 게임/디펜스 게임.java
@@ -11,18 +11,11 @@ class Solution {
             if(n < enemy[i] && k == 0) break;   // 병사가 부족하고 무적권도 없을 경우
             pq.offer(enemy[i]); // 적군 수 우선순위 큐에 추가
             if(n < enemy[i]) {  // 병사가 부족해서 무적권을 사용할 경우
-                // if(pq.size() == 0) {    // 첫 판인 경우
-                //     n += enemy[i];
-                // } else {    // 첫 판이 아닐 경우
-                //     n += pq.poll(); // 이 전에 가장 많았던 적군 수 만큼 채우기
-                // }
-                n += pq.poll(); // 이 전에 가장 많았던 적군 수 만큼 채우기
+                n += pq.poll(); // 지금까지 가장 많았던 적군 수 만큼 채우기 (지금 라운드 포함)
                 k --;   // 무적권 사용
-                // System.out.println("무적권 사용 " + i + "번째 " + k + " " + n + " " + enemy[i]);
             }
             n -= enemy[i];  // 병사 사용
             answer ++;  // 라운드 추가
-            // System.out.println(i + "번째 " + k + "개의 무적권 " + n + "병사 " + enemy[i] + "현재적군 " + answer + "라운드");
         }
         
         return answer;
