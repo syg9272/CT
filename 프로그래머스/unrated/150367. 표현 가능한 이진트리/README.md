9d42e7a313251b2704e69fb9392e66472e3ca1f6
Seo Yeji <syg9272@naver.com>
2023-09-16 17:43:31 +0900

    [unrated] Title: 표현 가능한 이진트리, Time: 89.14 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/unrated/150367. 표현 가능한 이진트리/README.md
+++ 프로그래머스/unrated/150367. 표현 가능한 이진트리/README.md
@@ -1,10 +1,10 @@
 # [unrated] 표현 가능한 이진트리 - 150367 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150367#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/150367) 
 
 ### 성능 요약
 
-메모리: 122 MB, 시간: 89.57 ms
+메모리: 109 MB, 시간: 89.14 ms
 
 ### 구분
 
--- 프로그래머스/unrated/150367. 표현 가능한 이진트리/표현 가능한 이진트리.java
+++ 프로그래머스/unrated/150367. 표현 가능한 이진트리/표현 가능한 이진트리.java
@@ -15,17 +15,18 @@ class Solution {
             }
             // 포화 이진트리로 만들기 (2^n - 1)
             int n = 1;
+            // 부족한 노드 개수 찾기
             while(n < binaryNum.length()){
                 n = n * 2 + 1;
             }
+            // 부족한 만큼 앞에 0 채우기 (값이 변하지 않으면서 노드 채우기)
             while(binaryNum.length() < n) {
                 binaryNum = "0" + binaryNum;
             }
-            // if(binaryNum.length() % 2 == 0) binaryNum = "0" + binaryNum;
             // 리프노드까지 순회
             boolean flag = true;    // 이진트리 표현 가능 여부
             int parent = binaryNum.length() / 2;    // 부모노드 위치
-            if(parent == 0) {
+            if(parent == 0) {   // 1자리 수 예외처리
                 answer[i] = 1;
                 continue;
             }
@@ -33,32 +34,21 @@ class Solution {
             q.offer(new String[] {binaryNum.substring(parent, parent + 1),  // 부모 노트
                                   binaryNum.substring(0, parent),           // 왼쪽 자식 그룹
                                   binaryNum.substring(parent + 1)});         // 오른쪽 자식 그룹
-            // System.out.println(binaryNum + " " + numbers[i]);
-            // System.out.print(binaryNum.substring(parent, parent + 1) + " ");
-            // System.out.print(binaryNum.substring(0, parent) + " ");
-            // System.out.print(binaryNum.substring(parent + 1));
-            // System.out.println();
             outer : while(!q.isEmpty()) {
                 // 자식노드가 1로 존재할 때 부모노드가 0인 경우 찾기
                 int time = q.size();
                 for(int t = 0; t < time; t ++) {
-                    String[] temp = q.poll();
+                    String[] temp = q.poll();   // 현재 그룹
                     for(int c = 1; c < temp.length; c ++) {
-                        int idx = temp[c].length() / 2;
-                        // System.out.println("부모와 자식 우두머리" + temp[0] + " " + temp[c].substring(idx, idx + 1));
-                        // System.out.print(temp[c].substring(idx, idx + 1) + " ");
-                        // System.out.print(temp[c].substring(0, idx) + " ");
-                        // System.out.print(temp[c].substring(idx + 1));
-                        // System.out.println();
-                        
-                        if(temp[0].equals("0") && temp[c].substring(idx, idx + 1).equals("1")) {
+                        int idx = temp[c].length() / 2; // 자식그룹의 가운데 노드 (다음 자식들의 부모)
+                        if(temp[0].equals("0") && temp[c].substring(idx, idx + 1).equals("1")) {    // 부모 0 자식 1
                             flag = false;
-                            break outer;
+                            break outer;    // 종료
                         }
-                        if(idx != 0) {
-                            q.offer(new String[] {temp[c].substring(idx, idx + 1),
-                                                 temp[c].substring(0, idx),
-                                                 temp[c].substring(idx + 1)});
+                        if(idx != 0) {  // 다음 자식그룹이 있다면
+                            q.offer(new String[] {temp[c].substring(idx, idx + 1),  // 부모
+                                                 temp[c].substring(0, idx),         // 왼쪽 자식그룹
+                                                 temp[c].substring(idx + 1)});      // 오른쪽 자식그룹
                         }
                     }
                 }
