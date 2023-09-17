3654a03481f4f8c3a75730ba7246e4f2fbb45f71
Seo Yeji <syg9272@naver.com>
2023-09-17 15:02:54 +0900

    [unrated] Title: 귤 고르기, Time: 66.50 ms, Memory: 138 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.java
@@ -2,27 +2,22 @@ import java.util.*;
 
 class Solution {
     public int solution(int k, int[] tangerine) {
-        // 크기별 카운트 저장할 해시맵
-        Map<Integer, Integer> map = new HashMap<>();
-        // 정렬된 귤 배열 카운트
+        // 크기별 카운트 저장할 배열
+        int[] count = new int[10000001];
+        // 귤 개수 카운트
         for(int i = 0; i < tangerine.length; i ++) {
-            if(map.containsKey(tangerine[i])) {
-                map.replace(tangerine[i], map.get(tangerine[i]) + 1);
-            } else {
-                map.put(tangerine[i], 1);
-            }
+            count[tangerine[i]] ++;
         }
         // 카운트 값 정렬
-        List<Integer> list = new ArrayList<>(map.values()); // 리스트에 value 옮기기
-        Collections.sort(list, Collections.reverseOrder()); // 내림차순 정렬
+        Arrays.sort(count); // 내림차순 정렬
         // 리스트 돌면서 종류 구하기
         int cnt = 1;    // 종류
         int sum = 0;    // 귤 개수
-        for(int i = 0; i < list.size(); i ++) {
-            if(sum + list.get(i) >= k) {
+        for(int i = count.length - 1; i >= 0; i --) {
+            if(sum + count[i] >= k) {
                 break;
             } else {
-                sum += list.get(i);
+                sum += count[i];
                 cnt ++;
             }
         }
