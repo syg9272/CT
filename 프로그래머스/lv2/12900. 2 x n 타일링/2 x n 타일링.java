37a553203c27698546ab47c3d7536ffc58f3c384
Seo Yeji <syg9272@naver.com>
2023-09-27 23:09:33 +0900

    [level 2] Title: 2 x n 타일링, Time: 9.43 ms, Memory: 54.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12900. 2 x n 타일링/2 x n 타일링.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n) {
+        long[][] map = new long[n][4];
+        map[0][0] = map[0][3] = 1;
+        
+        for(int row = 0; row < n - 1; row ++) {
+            map[row + 1][0] += (map[row][3] + map[row][0]) % 1000000007L;
+            map[row + 1][3] += map[row][0] % 1000000007L;
+            map[row + 1][1] += map[row][2] % 1000000007L;
+            map[row + 1][2] += map[row][1] % 1000000007L;
+        }
+        
+        return (int) (map[n - 1][0] % 1000000007L);
+    }
+}
