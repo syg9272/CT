ce418f70c9bec10534255a2261369e1be58e2aec
Seo Yeji <syg9272@naver.com>
2023-09-27 22:28:17 +0900

    [level 2] Title: 3 x n 타일링, Time: 4.09 ms, Memory: 52.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12902. 3 x n 타일링/3 x n 타일링.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(int n) {
+        // 효율적인 연산을 위해 직사각형을 90도 회전
+        long[][] map = new long[n][8];    // 한 행에 나올 수 있는 경우의 수 8가지
+        map[0][1] = map[0][4] = map[0][7] = 1;  // 1번째 행에서 가능한 경우
+        // DP 연산
+        for(int i = 0; i < n - 1; i ++) {
+            for(int now = 0; now < 8; now ++) { // 현재 행
+                map[i + 1][now ^ 7] += map[i][now] % 1000000007L; // 규칙 1
+                if(now == 1 || now == 4) {  // 규칙 2
+                    map[i + 1][0] += map[i][now] % 1000000007L;  
+                }
+                if(now == 0) {  // 규칙 2
+                    map[i + 1][1] += map[i][0] % 1000000007L;
+                    map[i + 1][4] += map[i][0] % 1000000007L;
+                }
+            }
+        }
+        // n행의 유일한 유효값인 000에 저장된 결과값 반환
+        // return (int) map[n - 1][0];
+        return (int) (map[n - 1][0] % 1000000007L);
+    }
+}
