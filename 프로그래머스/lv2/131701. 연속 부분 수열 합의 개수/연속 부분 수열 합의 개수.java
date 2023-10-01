7f8efb1b657ce0e568329c7112657ed6307f96a8
Seo Yeji <syg9272@naver.com>
2023-10-01 17:58:50 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 111.82 ms, Memory: 140 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] elements) {
         // 누적합 저장 배열 선언 (원형 배열 구현을 위해 elements.length * 2, 누적합 저장을 위해 + elements.length)
@@ -7,19 +9,14 @@ class Solution {
             prefixSum[i] = prefixSum[i - 1] + elements[i % elements.length];
         }
         // 중복 제거를 위해 해시셋에 부분 수열 합 저장
-        boolean[] checked = new boolean[1000 * 1000 + 1];
+        HashSet<Long> hSet = new HashSet<>();
         // 1 - element.length 까지 부분 수열 합 구하기
         for(int i = 1; i <= elements.length; i ++) {
             for(int j = elements.length; j < prefixSum.length; j ++) {
-                checked[(int) (prefixSum[j] - prefixSum[j - i])] = true;
+                hSet.add(prefixSum[j] - prefixSum[j - i]);
             }
         }
-        // 체크된 경우 카운트
-        int answer = 0;
-        for(int i = 0; i < checked.length; i ++) {
-            if(checked[i]) answer ++;
-        }
         // 해시셋 사이즈 반환
-        return answer;
+        return hSet.size();
     }
 }
