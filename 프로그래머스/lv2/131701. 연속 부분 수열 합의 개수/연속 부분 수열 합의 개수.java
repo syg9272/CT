fb7b8f4c2852ecb67d1e7bd17b3df1fb11a3b107
Seo Yeji <syg9272@naver.com>
2023-10-01 17:57:39 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 13.27 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -1,36 +1,25 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[] elements) {
-        // int answer = 0;
-        
-        // long[] num = new long[elements.length * 2];
+        // 누적합 저장 배열 선언 (원형 배열 구현을 위해 elements.length * 2, 누적합 저장을 위해 + elements.length)
         long[] prefixSum = new long[elements.length * 3 + 1];
-        
-        // // 원형 배열을 구현하기 위해 길이 두 배 배열 생성
-        // for(int i = 0; i < num.length; i ++) {
-        //     num[i] = elements[i % elements.length];
-        // }
         // 누적합 저장
         for(int i = elements.length; i < prefixSum.length; i ++) {
             prefixSum[i] = prefixSum[i - 1] + elements[i % elements.length];
         }
-        
-        // System.out.println(Arrays.toString(prefixSum));
-        
-        // boolean[] checked = new boolean[1000 * 1000 + 1];
-        HashSet<Long> hSet = new HashSet<>();
-        
+        // 중복 제거를 위해 해시셋에 부분 수열 합 저장
+        boolean[] checked = new boolean[1000 * 1000 + 1];
         // 1 - element.length 까지 부분 수열 합 구하기
         for(int i = 1; i <= elements.length; i ++) {
             for(int j = elements.length; j < prefixSum.length; j ++) {
-                // System.out.println(j + " " + (j - i));
-                // System.out.print(prefixSum[j] - prefixSum[j - i] + " ");
-                // checked[(int) (prefixSum[j] - prefixSum[j - i])] = true;
-                hSet.add(prefixSum[j] - prefixSum[j - i]);
+                checked[(int) (prefixSum[j] - prefixSum[j - i])] = true;
             }
         }
-        
-        return hSet.size();
+        // 체크된 경우 카운트
+        int answer = 0;
+        for(int i = 0; i < checked.length; i ++) {
+            if(checked[i]) answer ++;
+        }
+        // 해시셋 사이즈 반환
+        return answer;
     }
 }
