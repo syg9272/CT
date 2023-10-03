0229b7013e8a4140778ac553fff12ebe0edbc4b8
Seo Yeji <syg9272@naver.com>
2023-10-03 13:14:54 +0900

    [unrated] Title: 숫자 카드 나누기, Time: 0.98 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/unrated/135807. 숫자 카드 나누기/숫자 카드 나누기.java
+++ 프로그래머스/unrated/135807. 숫자 카드 나누기/숫자 카드 나누기.java
@@ -20,43 +20,40 @@ class Solution {
     // 공약수 찾기
     public void findDivisor(int[] card) {
         for(int j = 2; j * j <= card[0]; j ++) {  // 0번째 수(가장 작은 수)의 약수 list에 넣기
-            if(card[0] % j == 0) {
+            if(card[0] % j == 0) {  // 약수일 경우, 나누는 요소와 몫 동시에 담기
                 listDivisor.add(j);
                 listDivisor.add(card[0] / j);
             }
         }
-        listDivisor.add(card[0]);
-        Collections.sort(listDivisor, Collections.reverseOrder());
+        listDivisor.add(card[0]);   // 자기 자신 약수로 넣기
+        Collections.sort(listDivisor, Collections.reverseOrder());  // 내림차순 정렬 (최대값 찾아야 하므로)
         return;
     }
     
+    // 모든 카드가 나누어 떨어져야 하는 경우
     public boolean findCanDiv(int[] canDiv, int divisor) {
         for(int num : canDiv) {
             if(num % divisor != 0) return false;
         }
-        
         return true;
     }
     
+    // 모든 카드가 나누어 떨어지면 안되는 경우
     public boolean findCantDiv(int[] cantDiv, int divisor) {
         for(int num : cantDiv) {
             if(num % divisor == 0) return false;
         }
-        
         return true;
     }
     
     // 조건 만족하는 최대값 찾기
     public int findMaxValue(int[] canDiv, int[] cantDiv) {
-        // 공약수 구하기
-        listDivisor = new ArrayList<>();
-        // 각 카드셋의 공약수 찾기
-        findDivisor(canDiv);
-        // 조건 만족하는 공약수 반환
-        for(Integer num : listDivisor) {
+        listDivisor = new ArrayList<>();    // 공약수 저장 리스트
+        findDivisor(canDiv);    // 공약수 찾기
+        
+        for(Integer num : listDivisor) {    // 조건 만족하는 공약수 반환
             if(findCanDiv(canDiv, num) && findCantDiv(cantDiv, num)) return num;
         }
-        
-        return 0;
+        return 0;   // 만족하는 공약수가 없을 경우 0 반환
     }
 }
